bc1f810ffaa61de7249ab51bc6b735954bad90b0
WangLeto <wanglitong@bytedance.com>
2021-01-05 14:48:55 +0800

    feat(index.d.ts): add typescript support

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+declare module 'waiter-promise' {
+  class Waiter<T, K> {
+    public readonly promise: Promise<any>
+    public readonly then: Promise<T>['then']
+    public readonly result: T
+    public readonly reason: K
+    public readonly resolve: (result: T) => void
+    public readonly reject: (reason: K) => void
+    public readonly settled: boolean
+    public readonly catch: Promise<K>['catch']
+  }
+  export = Waiter
+}
